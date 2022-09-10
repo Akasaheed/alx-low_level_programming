5694d55a5c9889f37c8587dd8a7c3b14a9728205
Akasaheed <Akasaheed@gmail.com>
2022-09-10 15:30:41 -0700

    c

---
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/* more header goes there /*
+
+/* betty style for functions may goed there /*
+int main(void)
+{
+ 	int n;
+
+	srand(time(0));
+	n = rand() -RAND_mAX / 2;
+	/* your code goes there */
+	return (0);
+}
+
