d203f574308980e785180de4dfc5c6117180bbb8
Akasaheed <Akasaheed@gmail.com>
2022-09-15 16:49:20 -0700

    c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - print a square
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+		_putchar(10);
+
+	j = size;
+	while (size > 0)
+	{
+		i = j;
+		while (i > 0)
+		{
+			_putchar('#');
+			i--;
+		}
+		_putchar(10);
+		size--;
+	}
+}
+
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * fizzbuzz - print fizzbuzz
+ */
+void fizzbuzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s", "FizzBuzz");
+		}
+
+		else if ((i % 3 == 0) && (i % 5 != 0))
+		{
+			printf("%s", "Fizz");
+		}
+		else if ((i % 3 != 0) && (i % 5 == 0))
+		{
+			printf("%s", "Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		putchar(32);
+}
